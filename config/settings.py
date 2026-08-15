0edf2940c6cb0bd19464652d620f9c4c09a35515
vishalidodda22@gmail.com <vishalidodda22@gmail.com>
2026-08-15 14:29:53 +0530

    feat : configure CORS for frontend integration

--- config/settings.py
+++ config/settings.py
@@ -41,6 +41,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     "django_filters",
+
+    "corsheaders",
+
     "rest_framework",
     "users",
     "products",
@@ -50,6 +53,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
